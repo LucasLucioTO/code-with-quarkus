06439681f5b422fcdcb57347b5886d6d572ef374
LucasLucioTO <lucasluciot.deoliveira@gmail.com>
2022-12-22 21:55:49 -0300

    fix: add attribute clientId

--- src/main/java/styluscar/car/Car.java
+++ src/main/java/styluscar/car/Car.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -30,6 +31,11 @@ public class Car {
 
     @Column(name = "plate")
     private String plate;
+
+    @Column(name = "clientId")
+    private Long clientId;
+
+
 
     public void merge(Car car) {
         this.setModel(car.getModel());
