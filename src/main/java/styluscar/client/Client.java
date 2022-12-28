e9a1483c6f891239eb050504ed87b0ae8ad83575
LucasLucioTO <lucasluciot.deoliveira@gmail.com>
2022-12-28 18:17:35 -0300

    feat: created the merge and buildClient methods

--- src/main/java/styluscar/client/Client.java
+++ src/main/java/styluscar/client/Client.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import styluscar.client.dto.CreateClientDto;
+import styluscar.client.dto.UpdateClientDto;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
@@ -23,4 +25,16 @@ public class Client {
     private String phone;
     @Column(name="status")
     private Boolean status;
+
+    public void merge(UpdateClientDto updateClientDto){
+        this.setName(updateClientDto.getName());
+        this.setPhone(updateClientDto.getPhone());
+        this.setStatus(updateClientDto.getStatus());
+    }
+    public void buildClient(CreateClientDto createClientDto){
+        this.setName(createClientDto.getName());
+        this.setPhone(createClientDto.getPhone());
+        this.setStatus(createClientDto.getStatus());
+
+    }
 }
