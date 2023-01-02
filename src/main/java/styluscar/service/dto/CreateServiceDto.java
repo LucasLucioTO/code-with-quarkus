943cdba61f18b5d902fb58d091aa1eb5992b125b
LucasLucioTO <lucasluciot.deoliveira@gmail.com>
2023-01-02 18:36:17 -0300

    feat: create Services DTO's

---
+++ src/main/java/styluscar/service/dto/CreateServiceDto.java
@@ -0,0 +1,27 @@
+package styluscar.service.dto;
+
+
+import io.smallrye.common.constraint.NotNull;
+import lombok.Data;
+
+@Data
+public class CreateServiceDto {
+
+    @NotNull
+    private String type;
+
+    @NotNull
+    private boolean status;
+
+    @NotNull
+    private Long carId;
+
+    @NotNull
+    private Long functionaryId;
+
+    @NotNull
+    private Long materialId;
+
+    @NotNull
+    private Long quantityMaterial;
+}
