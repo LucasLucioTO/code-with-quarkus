9723ef5f41328f4ceeabfbaea5b9b65c8fdadded
LucasLucioTO <lucasluciot.deoliveira@gmail.com>
2023-01-30 23:11:54 -0300

    fix: remove comments and imports

--- src/main/java/styluscar/client/Client.java
+++ src/main/java/styluscar/client/Client.java
@@ -1,19 +1,11 @@
 package styluscar.client;
 
-import javax.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import styluscar.client.dto.CreateClientDto;
-import styluscar.client.dto.UpdateClientDto;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -27,16 +19,4 @@ public class Client {
     private String phone;
     @Column(name="status")
     private Boolean status;
-
-    public void merge(UpdateClientDto updateClientDto){
-        this.setName(updateClientDto.getName());
-        this.setPhone(updateClientDto.getPhone());
-        this.setStatus(updateClientDto.getStatus());
-    }
-    public void buildClient(CreateClientDto createClientDto){
-        this.setName(createClientDto.getName());
-        this.setPhone(createClientDto.getPhone());
-        this.setStatus(createClientDto.getStatus());
-
-    }
 }
